ae5939dd9facb46a2872a6d690f8eb3ea9c22902
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-10-27 00:22:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+
+        // Function to add "DOM load success" to the page
+        function addTextToPage() {
+            const text = document.createElement("p");
+            text.textContent = "DOM load success";
+            document.body.appendChild(text);
+        }
+
+        // Add an event listener to call the function when the DOM is loaded
+        document.addEventListener("DOMContentLoaded", addTextToPage);
